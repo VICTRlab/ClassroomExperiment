7320fc22bc9f84b0969c90b3200f5a9aa7f18471
michaeleeli <michaeleeli@hotmail.com>
2017-01-30 15:46:17 -0800

    Updates

---
+++ Assets/Scripts/BatteryLifeScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BatteryLifeScript : MonoBehaviour {
+
+    GameObject BatteryLifeImage;
+    float charge = 10; // Random initial number. Range should be 2 - 20.
+
+	// Use this for initialization
+	void Start () {
+        BatteryLifeImage = this.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        charge = Random.Range(9f, 11f); // smaller range right now
+        gameObject.transform.localScale = new Vector3(charge / 20, gameObject.transform.localScale.y, gameObject.transform.localScale.z);
+
+	}
+}
---
+++ Assets/Scripts/HeartBeatScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeartBeatScript : MonoBehaviour {
+
+    GameObject HeartImage;
+    public float beatsPerMin = 90; // Default is 90bpm.
+    float beatTime = 0.666666667f; // Default is 90bpm, so 90 beats / 60 seconds = 1.5 beats per sec, or 0.6666667sec per beat
+    Vector3 originalImageScale;
+
+	// Use this for initialization
+	void Start () {
+        HeartImage = this.gameObject;
+        originalImageScale = this.transform.localScale;
+        SetBPMtoBeatTime(beatsPerMin);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        beatTime -= Time.deltaTime;
+        Debug.Log(beatTime);
+        HeartImage.transform.localScale = Vector3.Lerp(originalImageScale/1.5f, originalImageScale, beatTime);
+        if ( beatTime < 0)
+        {
+            HeartImage.transform.localScale = originalImageScale;
+            SetBPMtoBeatTime(beatsPerMin);
+        }
+
+	}
+
+    void SetBPMtoBeatTime(float num)
+    {
+        beatTime = (1 / (beatsPerMin / 60));
+    }
+}
