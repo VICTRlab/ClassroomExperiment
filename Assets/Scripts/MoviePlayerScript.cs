4f5d9385fd091e73877a704b7871870672bab20b
michaeleeli <michaeleeli@hotmail.com>
2017-01-31 18:03:24 -0800

    Video

---
+++ Assets/Scripts/MoviePlayerScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoviePlayerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        Renderer r = GetComponent<Renderer>();
+        MovieTexture movie = (MovieTexture)r.material.mainTexture;
+
+        movie.Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
