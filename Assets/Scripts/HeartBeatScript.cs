cf014be762a796038bb238ac5d55df988e22e4a5
michaeleeli <michaeleeli@hotmail.com>
2017-02-17 14:47:49 -0800

    Updates plus todo log

--- Assets/Scripts/HeartBeatScript.cs
+++ Assets/Scripts/HeartBeatScript.cs
@@ -4,8 +4,9 @@ using System.Collections;
 public class HeartBeatScript : MonoBehaviour {
 
     GameObject HeartImage;
-    public float beatsPerMin = 90; // Default is 90bpm.
+    public float beatsPerMin = 90; // Default is 90bpm. This is the value you want to input to change the heart beat's speed.
     float beatTime = 0.666666667f; // Default is 90bpm, so 90 beats / 60 seconds = 1.5 beats per sec, or 0.6666667sec per beat
+    float countDown = 0.666666667f;
     Vector3 originalImageScale;
 
 	// Use this for initialization
@@ -19,12 +20,13 @@ public class HeartBeatScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        beatTime -= Time.deltaTime;
-        HeartImage.transform.localScale = Vector3.Lerp(originalImageScale/1.5f, originalImageScale, beatTime);
-        if ( beatTime < 0)
+        countDown -= Time.deltaTime / beatTime;
+        HeartImage.transform.localScale = Vector3.Lerp(originalImageScale/1.5f, originalImageScale, countDown);
+        if ( countDown < 0)
         {
             HeartImage.transform.localScale = originalImageScale;
             SetBPMtoBeatTime(beatsPerMin);
+            countDown = beatTime;
         }
 
 	}
---
+++ Assets/Scripts/SceneControllerManagerScript.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+// This script is used to control behavior in the scene; currently you can toggle the portrait, avatar model, and video/text on the projector screen.
+
+public class SceneControllerManagerScript : MonoBehaviour {
+
+    public GameObject UIPortraitObject;
+    Image UIPortrait;
+    public Sprite standardPortrait;
+    public Sprite personalizedPortrait;
+    bool standardPort = true;
+
+    public GameObject StandardAvatar;
+    public GameObject PersonalizedAvatar;
+    bool standardAvat = true;
+
+    public GameObject Projector;
+    MeshRenderer ProjectorDisplay;
+    public Material movie;
+    public Material wikitext;
+    bool standardProjector = true;
+
+	// Use this for initialization
+	void Start () {
+
+        UIPortrait = UIPortraitObject.GetComponent<Image>();
+        
+        // Turn off all child renderer from StandardAvatar's childrens
+        foreach (Renderer r in StandardAvatar.GetComponentsInChildren<Renderer>())
+            r.enabled = false;
+
+        ProjectorDisplay = Projector.GetComponent<MeshRenderer>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("1"))
+        {
+            switchPortrait(standardPort);
+            standardPort = !standardPort;
+        }
+
+        if (Input.GetKeyDown("2"))
+        {
+            switchAvatar(standardAvat);
+            standardAvat = !standardAvat;
+        }
+
+        if (Input.GetKeyDown("3"))
+        {
+            switchProjector(standardProjector);
+            standardProjector = !standardProjector;
+        }
+	
+	}
+
+    void switchPortrait(bool standardPort)
+    {
+        if (standardPort)
+            UIPortrait.sprite = standardPortrait;
+        else
+            UIPortrait.sprite = personalizedPortrait;
+    }
+
+    void switchAvatar(bool standardAvat)
+    {
+        if (standardAvat)
+        {
+            foreach (Renderer r in StandardAvatar.GetComponentsInChildren<Renderer>())
+                r.enabled = true;
+            foreach (Renderer r in PersonalizedAvatar.GetComponentsInChildren<Renderer>())
+                r.enabled = false;
+        }
+        else
+        {
+            foreach (Renderer r in StandardAvatar.GetComponentsInChildren<Renderer>())
+                r.enabled = false;
+            foreach (Renderer r in PersonalizedAvatar.GetComponentsInChildren<Renderer>())
+                r.enabled = true;
+        }
+    }
+
+    void switchProjector(bool standardProjector)
+    {
+        if (standardProjector)
+            ProjectorDisplay.material = wikitext;
+        else
+            ProjectorDisplay.material = movie;
+    }
+}
