f32ce5752eb119d61d2aaf2f2248aa90fdfddb5e
toothmang <wangninja@gmail.com>
2018-08-15 00:21:00 -0700

    Adding Menu and Experiment scripts, imported TriLib (paid) and StandaloneFileDialog (free) plugins

---
+++ Assets/Scripts/Experiment.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Experiment : MonoBehaviour {
+
+    public static Experiment Instance;
+
+    public enum Progress
+    {
+        Begin,
+        SelectMode,
+        SelectAvatar,
+        InClassroom,
+        Seated,
+        End
+    }
+
+    public Progress CurrentProgress = Progress.Begin;
+
+    public GameObject participantAvatar;
+
+	// Use this for initialization
+	void Start () {
+        Instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadParticipantAvatar(string fbxPath)
+    {
+        using (var assetLoader = new TriLib.AssetLoader())
+        {
+            try
+            {
+                var loaderOptions = new TriLib.AssetLoaderOptions();
+                loaderOptions.DontLoadCameras = true;
+                loaderOptions.DontLoadLights = true;
+                participantAvatar = assetLoader.LoadFromFile(fbxPath, loaderOptions);
+            }
+            catch(Exception ex)
+            {
+                Debug.LogError(ex.ToString());
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/IMMenu.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SFB;
+
+public class IMMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnGUI()
+    {
+        //if (Experiment.Instance.CurrentProgress == Experiment.Progress.Begin)
+        {
+            if (GUILayout.Button("Load model"))
+            {
+                Debug.Log("Pressed!");
+                var selectedFile = StandaloneFileBrowser.OpenFilePanel("Choose participant avatar", "", "fbx", false);
+
+                if (selectedFile.Length > 0)
+                {
+                    var fileInfo = new FileInfo(selectedFile[0]);
+
+                    if (fileInfo.Exists)
+                    {
+                        Experiment.Instance.LoadParticipantAvatar(fileInfo.FullName);
+                    }
+                }
+            }
+        }
+    }
+}
